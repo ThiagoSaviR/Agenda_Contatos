848f921f2ce681d9a620d5fdc9acadad0772bdde
ThiagoSaviR <thiagosavir@gmail.com>
2022-08-05 20:03:52 -0300

    feature: create-react-app, install dependecies and create data base with Firebase

---
+++ src/dataBase/firebase.jsx
@@ -0,0 +1,13 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDFQ38e77NxiVmGHhlsIAn6FrfZDVg7UE0",
+  authDomain: "agenda-cbd2f.firebaseapp.com",
+  projectId: "agenda-cbd2f",
+  storageBucket: "agenda-cbd2f.appspot.com",
+  messagingSenderId: "743594711814",
+  appId: "1:743594711814:web:064c383f1d55d914095c87"
+};
+const fireDB = firebase.initializeApp(firebaseConfig);
+
+export default fireDB.database().ref();
